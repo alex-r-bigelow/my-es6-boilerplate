6281bde0aca1dc68cc90b1d32701c450d1d13c16
Alex Bigelow <alex.r.bigelow@gmail.com>
2017-03-24 11:41:32 -0600

    Add example sass files and svg templates

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 import * as d3 from 'd3';
+import template from './template.svg';
 
-d3.select('body').text('ES6 Boilerplate');
+import './style.scss';
+
+function resize () {
+  let bounds = d3.select('#container').node().getBoundingClientRect();
+  let svg = d3.select('svg');
+  svg.attrs({
+    width: bounds.width,
+    height: bounds.height
+  });
+}
+
+function setup () {
+  d3.select('#container').html(template);
+  resize();
+}
+window.onload = setup;
+window.onresize = resize;
